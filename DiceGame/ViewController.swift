ac3ce11e6c84204ca2b6d8b23fae903cb9bedc01
genti <bytyqigentrit75@gmail.com>
2024-05-14 09:36:15 +0200

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by TokioMac on 14.5.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
